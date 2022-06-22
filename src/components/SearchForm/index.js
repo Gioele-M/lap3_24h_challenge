3814893c5cd26cb1042d6e4ae4b191e22ef7c9d8
Gioele-M <gioelemontis97@gmail.com>
2022-06-22 10:33:59 +0100

    complete form

---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+
+const SearchForm = () => {
+
+    const [user, setUser] = useState('')
+
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+    
+        // dispatch({type:'TITLE', payload: song})
+        // dispatch({type: 'ARTIST', payload:artist})
+
+        
+    }
+
+    const onUserChange = (e) => {
+        setUser(e.target.user)
+
+    }
+
+    return (
+
+
+        <div>   
+                <form onSubmit={onFormSubmit}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" value={user} onChange={onUserChange} />
+                    <button type="submit">submit</button>
+                </form>
+            </div>
+    
+
+    )
+
+
+}
+
+
+export default SearchForm
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+import { SearchForm } from "../../components";
+
 
 
 const Home = () => {
     return (
-        <h1>Home page</h1>
+        <>
+            <h1>Home page</h1>
+            <SearchForm />
+        </>
     )
 }
 
